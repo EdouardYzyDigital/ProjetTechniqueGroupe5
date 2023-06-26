42f269f0897ec9ee7bc9fc5ebd800fc988a72ec7
WinnMBG <winn77.m@hotmail.com>
2023-06-26 15:35:14 +0200

    create different views

---
+++ stock-maxairain/src/views/login/index.js
@@ -0,0 +1,19 @@
+import { Col } from "reactstrap";
+
+const Login = () => {
+    return (
+        <div className="container">
+            <h1 className="fs-1 text-center mt-4" style={{fontFamily: 'Roboto'}}>Login</h1>
+            <Col md='10'>
+                <div style={{backgroundColor: 'red'}}>
+
+                </div>
+            </Col>
+            <Col md='2'>
+
+            </Col>
+        </div>
+    );
+}
+
+export default Login;
---
+++ stock-maxairain/src/views/signin/index.js
@@ -0,0 +1,102 @@
+import { FormGroup, Col, Row, Input, Button, Card, CardHeader, CardBody, Label, Media } from 'reactstrap'
+import { useState } from 'react';
+import logoDefault from '../../images/default.png'
+
+const SignIn = () => {
+    const [image, OnChangeImage] = useState(null)
+    const [name, setName] = useState(null)
+    const [lastname, setLastName] = useState(null)
+    const [job, setJob] = useState(null)
+
+    return (
+        <div className="container">
+            <h1 className="fs-1 text-center mt-2" style={{fontFamily: 'Roboto', color: '#379EC1'}}>Sign In</h1>
+                <Card className='mt-3'>
+                <CardBody>
+                        <Col sm="12">
+                            <div className="divider">
+                            <div className="divider-text">Informations utilisateur</div>
+                            </div>
+                        </Col>
+                        <Row className='mt-3'>
+                            <Col md={6}>
+                            <FormGroup>
+                                <Label for="firstname">
+                                Fistname
+                                </Label>
+                                <Input
+                                defaultValue={name}
+                                id="firstname"
+                                name="firstname"
+                                placeholder="Enter your firstname..."
+                                onChange={setName}
+                                />
+                            </FormGroup>
+                            </Col>
+                            <Col md={6}>
+                            <FormGroup>
+                                <Label for="lastname">
+                                Lastname
+                                </Label>
+                                <Input
+                                defaultValue={lastname}
+                                id="lastname"
+                                name="lastname"
+                                placeholder="Enter your lastname..."
+                                onChange={setLastName}
+                                />
+                            </FormGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                        <Col md={6}>
+                            <FormGroup>
+                                <Label for="poste">
+                                Poste
+                                </Label>
+                                <Input
+                                defaultValue={job}
+                                id="poste"
+                                name="poste"
+                                placeholder="Enter your job..."
+                                type="text"
+                                onChange={setJob}
+                                />
+                            </FormGroup>
+                        </Col>
+                        <Col md={6}>
+                            <FormGroup>
+                            <Media className="mt-4 ml-1" body>
+                                {image === null ? <img src={logoDefault} alt=''/> : <img src={image} alt=''/>}
+                                <div className='d-flex mt-3 align-items-center' style={{gap: '10px'}}>
+                                    <Button
+                                        tag={Label}
+                                        className="mr-75"
+                                        size="sm"
+                                        color="primary"
+                                    >
+                                        Upload
+                                        <Input
+                                        type="file"
+                                        onChange={e => OnChangeImage(URL.createObjectURL(e.target.files[0]))}
+                                        hidden
+                                        accept="image/*"
+                                        />
+                                    </Button>
+                                    <p>Format autorisé JPG, GIF ou PNG. Taille max. de 500Ko</p>
+                                </div>
+                            </Media>
+                            </FormGroup>
+                        </Col>
+                        </Row>
+                        <Button color='btn btn-outline-info w-25 mt-2'>
+                            Sign in
+                        </Button>   
+                </CardBody>
+            </Card>
+        </div>
+
+    );
+}
+
+export default SignIn;

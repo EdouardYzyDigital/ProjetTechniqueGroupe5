bbc3a230fd6613fae6323747d82cb064d29fbc6d
WinnMBG <winn77.m@hotmail.com>
2023-06-26 15:33:16 +0200

    add router

--- stock-maxairain/src/App.js
+++ stock-maxairain/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Login from './views/login/';
+import SignIn from './views/signin';
+import Footer from './components/Footer';
+import  logo  from './images/MaxAirain2.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style={{fontFamily: 'Roboto'}}>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <img src={logo} alt="" style={{width:"250px", height:"75px", marginLeft:"30px", marginTop:"10px"}}/>
+      <Routes>
+          <Route
+            exact
+            path='/'
+            element={<Login/>}
+          />
+          <Route
+            exact
+            path='/signin'
+            element={<SignIn/>}
+          />
+          <Route
+            exact
+            path='*'
+            element={<h1>Error 404 not found ...</h1>}
+          />
+      </Routes>
+      <Footer/>
+    </>
   );
 }
 

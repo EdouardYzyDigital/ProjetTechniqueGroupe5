fda6d22c960614c4c9642d95b47a5bb53768ba81
WinnMBG <winn77.m@hotmail.com>
2023-06-27 16:13:41 +0200

    add swagger

--- back/src/index.js
+++ back/src/index.js
@@ -4,6 +4,9 @@ import { Model } from "objection"
 import knex from "knex"
 import allRoutes from "./routes/allRoutes.js"
 import bodyParser from "body-parser"
+import swaggerJsdoc from "swagger-jsdoc"
+import * as swaggerUi from "swagger-ui-express"
+import swaggerDocument from "./swagger.json" assert {type: "json"}
 
 const app = express()
 
@@ -12,6 +15,29 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }))
 app.use(express.json())
 const db = knex(knexfile)
 Model.knex(db)
+//   definition: {
+//     openapi: "3.1.0",
+//     info: {
+//       title: "MaxAirain Express API with Swagger",
+//       version: "0.1.0",
+//       description: "This is a simple CRUD API application made with Express and documented with Swagger"
+//     },
+//     servers: [
+//       {
+//         url: "http://localhost:3000",
+//       },
+//     ],
+//   },
+//   apis: ["./routes/*.js"],
+// };
+
+// const specs = swaggerJsdoc(options);
+app.use(
+  "/api",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, { explorer: true, customCssUrl:
+    "https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/theme-newspaper.css", })
+);
 const port = 3000
 
 allRoutes({ app, db })
